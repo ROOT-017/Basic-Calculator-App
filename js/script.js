51a504d8b473a13146b50c28e0b1efbe7b218e08
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2022-02-04 09:07:18 +0100

    On ready function added

--- js/script.js
+++ js/script.js
@@ -1,41 +1,56 @@
-function add_leading_zero(number) {
-    if (number < 10) {
-        return "0" + number.toString();
-    } else {
-        return number.toString();
+$(function() {
+    function add_leading_zero(number) {
+        if (number < 10) {
+            return "0" + number.toString();
+        } else {
+            return number.toString();
 
+        }
     }
-}
 
-function AM_PM(daytime) {
+    function AM_PM(daytime) {
 
-    if (daytime < 12) {
-        var time = "AM";
-        return time.toString();
-    } else {
-        time = "PM"
-        return time.toString();
+        if (daytime < 12) {
+            var time = "AM";
+            return time.toString();
+        } else {
+            time = "PM"
+            return time.toString();
+        }
     }
-}
+    var c = false
+        //Button T
 
+    $("#time").click(function() {
+        $("#text").css("display", "none")
+        $(".time").css("display", "inline")
+        window.setInterval(function() {
+            var currentTime = new Date()
+            var hours = currentTime.getHours();
+            var minutes = currentTime.getMinutes();
+            var seconds = currentTime.getSeconds();
+            document.getElementById("hours").innerHTML = add_leading_zero(hours) + ":";
+            document.getElementById("minutes").innerHTML = add_leading_zero(minutes) + " :";
+            document.getElementById("seconds").innerHTML = add_leading_zero(seconds);
+            document.getElementById("AM-PM").innerHTML = AM_PM(hours);
+        }, 1000);
+    })
 
-$(".box-1").click(function() {
-    $("#text").css("display", "none")
-    $(".time").css("display", "inline")
-    window.setInterval(function() {
-        var currentTime = new Date()
-        var hours = currentTime.getHours();
-        var minutes = currentTime.getMinutes();
-        var seconds = currentTime.getSeconds();
-        document.getElementById("hours").innerHTML = add_leading_zero(hours) + " :";
-        document.getElementById("minutes").innerHTML = add_leading_zero(minutes) + " :";
-        document.getElementById("seconds").innerHTML = add_leading_zero(seconds);
-        document.getElementById("AM-PM").innerHTML = AM_PM(hours);
-    }, 1000);
-})
-$(".box-2").click(function() {
-    $("#screen").text("")
-})
-$(".box-3").click(function() {
-    $("#screen").text("ROOT-CALCULATOR")
+    //Button C
+
+    $("#clear").click(function() {
+        $("#screen").text("0")
+    })
+
+    //Getting the value of the click button
+    var buttons = document.getElementsByClassName("box-")
+
+    for (var i = 0; i < buttons.length; i++) {
+        {
+            console.log(buttons[i].onclick)
+        }
+    }
+    $("#on_off").click(function() {
+        $("#screen").text("Hello")
+    })
 })
